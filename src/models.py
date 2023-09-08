92030fec99c556bd2be2d7da00bf70cc3778e0f6
javierjuan <javij1@gmail.com>
2023-09-08 13:55:12 +0200

    Layers:  * TransformerAttention  * TransformerFeedForward  * TransformerEncoder  * TransformerDecoder  * TokenAndPositionEmbedding  * PositionalEncoding (refactored)
    Models:
     * Transformer
    
    renamings

--- src/models.py
+++ src/models.py
@@ -20,6 +20,98 @@ class Model(tf.keras.Model):
             return {metric.name: metric.result() for metric in self.metrics}
 
 
+class Transformer(Model):
+    def __init__(self,
+                 input_vocabulary_size,
+                 output_vocabulary_size,
+                 embedding_dimension,
+                 dense_dimension,
+                 num_heads,
+                 num_encoders=1,
+                 num_decoders=1,
+                 positional='embedding',
+                 use_bias=True,
+                 output_shape=None,
+                 attention_axes=None,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 activation='mish',
+                 axis=-1,
+                 epsilon=1e-3,
+                 center=True,
+                 scale=True,
+                 beta_initializer='zeros',
+                 gamma_initializer='ones',
+                 beta_regularizer=None,
+                 gamma_regularizer=None,
+                 beta_constraint=None,
+                 gamma_constraint=None,
+                 embeddings_initializer='uniform',
+                 embeddings_regularizer=None,
+                 embeddings_constraint=None,
+                 mask_zero=False,
+                 input_length=None,
+                 sparse=False,
+                 rate=None,
+                 seed=None,
+                 name=None,
+                 **kwargs):
+        super().__init__(name=name, **kwargs)
+
+        self.input_embedding = layers.TokenAndPositionEmbedding(
+            vocabulary_size=input_vocabulary_size, embedding_dimension=embedding_dimension, positional=positional,
+            embeddings_initializer=embeddings_initializer, embeddings_regularizer=embeddings_regularizer,
+            activity_regularizer=activity_regularizer, embeddings_constraint=embeddings_constraint,
+            mask_zero=mask_zero, input_length=input_length, sparse=sparse, rate=rate, seed=seed)
+        self.output_embedding = layers.TokenAndPositionEmbedding(
+            vocabulary_size=output_vocabulary_size, embedding_dimension=embedding_dimension, positional=positional,
+            embeddings_initializer=embeddings_initializer, embeddings_regularizer=embeddings_regularizer,
+            activity_regularizer=activity_regularizer, embeddings_constraint=embeddings_constraint,
+            mask_zero=mask_zero, input_length=input_length, sparse=sparse, rate=rate, seed=seed)
+        self.encoders = [layers.TransformerEncoder(
+            embedding_dimension=embedding_dimension, dense_dimension=dense_dimension, num_heads=num_heads,
+            use_bias=use_bias, output_shape=output_shape, attention_axes=attention_axes,
+            kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint, activation=activation, axis=axis, epsilon=epsilon, center=center,
+            scale=scale, beta_initializer=beta_initializer, gamma_initializer=gamma_initializer,
+            beta_regularizer=beta_regularizer, gamma_regularizer=gamma_regularizer, beta_constraint=beta_constraint,
+            gamma_constraint=gamma_constraint, rate=rate, seed=seed) for _ in range(num_encoders)]
+        self.decoders = [layers.TransformerDecoder(
+            embedding_dimension=embedding_dimension, dense_dimension=dense_dimension, num_heads=num_heads,
+            use_bias=use_bias, output_shape=output_shape, attention_axes=attention_axes,
+            kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
+            bias_constraint=activity_regularizer, activation=activation, axis=axis, epsilon=epsilon, center=center,
+            scale=scale, beta_initializer=beta_initializer, gamma_initializer=gamma_initializer,
+            beta_regularizer=beta_regularizer, gamma_regularizer=gamma_regularizer, beta_constraint=beta_constraint,
+            gamma_constraint=gamma_constraint, rate=rate, seed=seed) for _ in range(num_decoders)]
+        self.posteriors = tf.keras.layers.Dense(
+            units=output_vocabulary_size, activation='softmax', use_bias=use_bias,
+            kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint)
+
+    def call(self, inputs, training=False, return_attention_scores=False, **kwargs):
+        context, outputs = inputs
+        context = self.input_embedding(context, training=training)
+        for encoder in self.encoders:
+            context = encoder(context, training=training,
+                              return_attention_scores=return_attention_scores)
+        for decoder in self.decoders:
+            outputs = decoder(outputs, context=context, training=training,
+                              return_attention_scores=return_attention_scores)
+        return self.posteriors(outputs)
+
+
 class ViT(Model):
     def __init__(self,
                  image_size,
@@ -46,7 +138,7 @@ class ViT(Model):
         self.flatten = tf.keras.layers.Flatten()
         self.dense_block_1 = layers.DenseBlock(units=units, normalization='layer', epsilon=epsilon)
         self.dense_block_2 = layers.DenseBlock(units=units // 4, normalization='layer', epsilon=epsilon)
-        self.posterior = tf.keras.layers.Dense(units=num_classes, activation='softmax')
+        self.posteriors = tf.keras.layers.Dense(units=num_classes, activation='softmax')
 
     def adapt(self, data, batch_size=None, steps=None):
         self.preprocessing.adapt(data=data, batch_size=batch_size, steps=steps)
@@ -61,4 +153,4 @@ class ViT(Model):
         x = self.flatten(x)
         x = self.dense_block_1(x)
         x = self.dense_block_2(x)
-        return self.posterior(x)
+        return self.posteriors(x)
