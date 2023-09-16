1e1ab912a2bce2f62de3b36f52b05c94cf13a9e9
javierjuan <javij1@gmail.com>
2023-09-16 21:46:14 +0200

    Add metrics.py

---
+++ src/metrics.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+
+class Dice(tf.keras.metrics.FBetaScore):
+    def __init__(self, average=None, beta=1.0, threshold=None, name=None):
+        super().__init__(average=average, beta=beta, threshold=threshold, name=name)
+
+
+class Jaccard(tf.keras.metrics.FBetaScore):
+    def __init__(self, average=None, beta=1.0, threshold=None, name=None):
+        super().__init__(average=average, beta=beta, threshold=threshold, name=name)
+
+    def result(self):
+        score = super().result()
+        return tf.math.divide_no_nan(score, (2 - score))
