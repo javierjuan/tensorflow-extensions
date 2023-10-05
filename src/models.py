dcba860cea79902fab581afddef2770b65233fa6
javierjuan <javij1@gmail.com>
2023-10-05 10:40:45 +0200

    cosmetics

--- src/models.py
+++ src/models.py
@@ -7,15 +7,15 @@ class Model(tf.keras.Model):
         x, y, sample_weight = data if len(data) == 3 else data + (None,)
 
         with tf.GradientTape() as tape:
-            with tf.name_scope('Forward'):
+            with tf.name_scope(name='Forward'):
                 y_pred = self(x, training=True)
                 with tf.name_scope('Loss'):
                     loss = self.compiled_loss(y, y_pred, sample_weight=sample_weight, regularization_losses=self.losses)
 
-        with tf.name_scope('Backward'):
+        with tf.name_scope(name='Backward'):
             self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
 
-        with tf.name_scope('Metrics'):
+        with tf.name_scope(name='Metrics'):
             self.compiled_metrics.update_state(y, y_pred, sample_weight=sample_weight)
             return {metric.name: metric.result() for metric in self.metrics}
 
