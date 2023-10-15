1db48d88260170a4e53a419e04ddfd366f41f49d
javierjuan <javij1@gmail.com>
2023-10-15 19:49:33 +0200

    Rename parameters to follow same convention

--- src/tensorflow_extensions/layers/patch.py
+++ src/tensorflow_extensions/layers/patch.py
@@ -4,36 +4,35 @@ import tensorflow as tf
 class PatchExtractor2D(tf.keras.layers.Layer):
     def __init__(self,
                  size,
-                 stride=None,
-                 rate=(1, 1),
+                 strides=None,
+                 rates=(1, 1),
                  padding='valid',
                  name=None,
                  **kwargs):
         super().__init__(name=name, **kwargs)
         if len(size) != 2:
             raise ValueError(f'`size` must be an array-like of size 2. Got: {size}')
-        if stride is not None and len(stride) != 2:
-            raise ValueError(f'`stride` must be an array-like of size 2. Got: {stride}')
-        if rate is not None and len(rate) != 2:
-            raise ValueError(f'`rate` must be an array-like of size 2. Got: {rate}')
+        if strides is not None and len(strides) != 2:
+            raise ValueError(f'`stride` must be an array-like of size 2. Got: {strides}')
+        if rates is not None and len(rates) != 2:
+            raise ValueError(f'`rate` must be an array-like of size 2. Got: {rates}')
 
         self.size = [1, *size, 1]
-        self.stride = self.size if stride is None else [1, *stride, 1]
-        self.rate = [1, 1, 1, 1] if rate is None else [1, *rate, 1]
+        self.strides = self.size if strides is None else [1, *strides, 1]
+        self.rates = [1, 1, 1, 1] if rates is None else [1, *rates, 1]
         self.padding = padding
         self.supports_masking = True
 
     def call(self, inputs, **kwargs):
-        patches = tf.image.extract_patches(images=inputs, sizes=self.size, strides=self.stride, rates=self.rate,
-                                           padding=self.padding.upper())
-        return tf.reshape(patches, shape=[-1, patches.shape[1] * patches.shape[2], patches.shape[3]])
+        return tf.image.extract_patches(images=inputs, sizes=self.size, strides=self.strides, rates=self.rates,
+                                        padding=self.padding.upper())
 
     def get_config(self):
         config = super().get_config()
         config.update({
             'size': self.size[1:-1],
-            'stride': self.stride[1:-1],
-            'rate': self.rate[1:-1],
+            'strides': self.strides[1:-1],
+            'rates': self.rates[1:-1],
             'padding': self.padding
         })
         return config
