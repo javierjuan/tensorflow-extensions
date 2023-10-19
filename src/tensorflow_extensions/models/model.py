ee40f25c2267950874ce9b8fd833a5ee5aa17968
javierjuan <javij1@gmail.com>
2023-10-19 17:25:37 +0200

    IMPORTANT * We must call self.compute_loss() and self.compute_metrics() methods instead of self.compiled_loss() and self.compiled_metrics() methods to allow override the normal behavior of loss and metrics computation (for custom losses)

--- src/tensorflow_extensions/models/model.py
+++ src/tensorflow_extensions/models/model.py
@@ -3,17 +3,16 @@ import tensorflow as tf
 
 class Model(tf.keras.Model):
     def train_step(self, data):
-        x, y, sample_weight = data if len(data) == 3 else data + (None,)
+        x, y, sample_weight = tf.keras.utils.unpack_x_y_sample_weight(data)
 
         with tf.GradientTape() as tape:
             with tf.name_scope(name='Forward'):
                 y_pred = self(x, training=True)
                 with tf.name_scope('Loss'):
-                    loss = self.compiled_loss(y, y_pred, sample_weight=sample_weight, regularization_losses=self.losses)
+                    loss = self.compute_loss(x=x, y=y, y_pred=y_pred, sample_weight=sample_weight)
 
         with tf.name_scope(name='Backward'):
             self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
 
         with tf.name_scope(name='Metrics'):
-            self.compiled_metrics.update_state(y, y_pred, sample_weight=sample_weight)
-            return {metric.name: metric.result() for metric in self.metrics}
+            return self.compute_metrics(x=x, y=y, y_pred=y_pred, sample_weight=sample_weight)
