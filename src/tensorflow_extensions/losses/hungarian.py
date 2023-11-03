1426e3324aca55ec42c2c7aa3194184e3fd979f3
javierjuan <javij1@gmail.com>
2023-11-03 21:42:49 +0100

    Rename loss from DETR to Hungarian

--- src/tensorflow_extensions/losses/hungarian.py
+++ src/tensorflow_extensions/losses/hungarian.py
@@ -21,15 +21,15 @@ def compute_bounding_box_loss(y_true, y_pred, assignment, padding_mask):
     return tf.math.reduce_sum(loss)
 
 
-class DETR(tf.keras.losses.Loss):
-    def __init__(self, labels_weight=1.0, bounding_boxes_weight=1.0, padding_axis=-1,
-                 reduction=tf.keras.losses.Reduction.AUTO, name='detr'):
+class Hungarian(tf.keras.losses.Loss):
+    def __init__(self, label_weight=1.0, bounding_box_weight=1.0, padding_axis=-1,
+                 reduction=tf.keras.losses.Reduction.AUTO, name='hungarian'):
         super().__init__(name=name, reduction=reduction)
-        self.matcher = HungarianMatcher(label_weight=labels_weight, bounding_box_weight=bounding_boxes_weight,
+        self.matcher = HungarianMatcher(label_weight=label_weight, bounding_box_weight=bounding_box_weight,
                                         padding_axis=padding_axis)
         self.padding_axis = padding_axis
-        self.label_weight = labels_weight
-        self.bounding_box_weight = bounding_boxes_weight
+        self.label_weight = label_weight
+        self.bounding_box_weight = bounding_box_weight
 
     def call(self, y_true, y_pred):
         zip_args = (tf.unstack(y_true['label']), tf.unstack(y_true['bounding_box']),
@@ -39,7 +39,7 @@ class DETR(tf.keras.losses.Loss):
             # Compute Hungarian matching
             assignment = self.matcher(y_true_label=y_true_label, y_true_bounding_box=y_true_bounding_box,
                                       y_pred_label=y_pred_label, y_pred_bounding_box=y_pred_bounding_box)
-            # Compute loss based on Hungarian matched pairs
+            # Compute loss based on Hungarian matching
             padding_mask = y_true_label[..., self.padding_axis]
             label_loss = compute_label_loss(y_true=y_true_label, y_pred=y_pred_label, assignment=assignment,
                                             padding_mask=padding_mask, padding_weight=0.1)
