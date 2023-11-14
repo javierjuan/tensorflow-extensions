b70b621de6bf9edca46d55011cffd2fe67450dbf
javierjuan <javij1@gmail.com>
2023-11-14 09:56:33 +0100

    * Improvements in Hungarian matcher * Implement compute_distance() method for bounding boxes

--- src/tensorflow_extensions/matchers/hungarian.py
+++ src/tensorflow_extensions/matchers/hungarian.py
@@ -12,21 +12,21 @@ def linear_sum_assignment(cost_matrix):
     return assignment
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
 def _permutation_indices(tensor):
     return tf.constant([1, 0]) if tf.rank(tensor) == 2 else tf.constant([0, 2, 1])
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
 def _compute_valid(bounding_box):
     y_min, x_min, y_max, x_max = tf.split(bounding_box[..., :4], 4, axis=-1)
     valid = tf.math.logical_and(tf.math.greater_equal(y_max, y_min), tf.math.greater_equal(x_max, x_min))
-    return tf.cast(valid, dtype=bounding_box.dtype)
+    return tf.cast(valid, bounding_box.dtype)
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
 def _compute_area(bounding_box):
     y_min, x_min, y_max, x_max = tf.split(bounding_box[..., :4], 4, axis=-1)
@@ -34,7 +34,7 @@ def _compute_area(bounding_box):
     return tf.math.maximum(tf.cast(0, bounding_box.dtype), area)
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
 def _compute_intersection(bounding_box_1, bounding_box_2):
     y_min1, x_min1, y_max1, x_max1 = tf.split(bounding_box_1[..., :4], 4, axis=-1)
@@ -50,7 +50,7 @@ def _compute_intersection(bounding_box_1, bounding_box_2):
     return intersect_height * intersect_width
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
 def _compute_convex_hull(bounding_box_1, bounding_box_2):
     y_min1, x_min1, y_max1, x_max1 = tf.split(bounding_box_1[..., :4], 4, axis=-1)
@@ -66,9 +66,9 @@ def _compute_convex_hull(bounding_box_1, bounding_box_2):
     return convex_hull_height * convex_hull_width
 
 
-# @tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
-def compute_giou(bounding_box_1, bounding_box_2, mode='giou'):
+def compute_iou(bounding_box_1, bounding_box_2, generalized=True):
     # Compute valid masks
     valid1 = _compute_valid(bounding_box_1)
     valid2 = _compute_valid(bounding_box_2)
@@ -77,38 +77,54 @@ def compute_giou(bounding_box_1, bounding_box_2, mode='giou'):
     bounding_box_2_area = _compute_area(bounding_box_2) * valid2
     # Compute valid matrix
     perm = _permutation_indices(bounding_box_2_area)
-    valid = tf.cast(tf.math.multiply(valid1, tf.transpose(valid2, perm)), bounding_box_1.dtype)
+    valid = tf.math.multiply(valid1, tf.transpose(valid2, perm))
     # Compute iou
     intersect_area = _compute_intersection(bounding_box_1, bounding_box_2) * valid
     union_area = (bounding_box_1_area + tf.transpose(bounding_box_2_area, perm)) - intersect_area
     iou = tf.math.divide_no_nan(intersect_area, union_area)
-    if mode == 'iou':
-        return iou
-    convex_hull_area = _compute_convex_hull(bounding_box_1, bounding_box_2) * valid
-    giou = iou - tf.math.divide_no_nan(convex_hull_area - union_area, convex_hull_area)
-    return giou
+    if generalized:
+        convex_hull_area = _compute_convex_hull(bounding_box_1, bounding_box_2) * valid
+        iou -= tf.math.divide_no_nan(convex_hull_area - union_area, convex_hull_area)
+    return iou
 
 
-# @tf.function
+@tf.function(reduce_retracing=True)
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
-def compute_matching(y_true_label, y_true_bounding_box, y_pred_label, y_pred_bounding_box, mode='giou'):
-    label_scores = 1.0 - tf.linalg.matmul(y_true_label, y_pred_label, transpose_b=True, a_is_sparse=True)
-    bounding_box_scores = 1.0 - compute_giou(y_true_bounding_box, y_pred_bounding_box, mode)
-    cost_matrix = label_scores + bounding_box_scores
+def compute_distance(bounding_box_1, bounding_box_2, norm=1):
+    # Compute valid masks
+    valid1 = _compute_valid(bounding_box_1)
+    valid2 = _compute_valid(bounding_box_2)
+    perm = _permutation_indices(bounding_box_2)
+    valid = tf.math.multiply(valid1, tf.transpose(valid2, perm))
+    # Compute distance
+    distance = tf.expand_dims(bounding_box_1, axis=-2) - tf.expand_dims(bounding_box_2, axis=-3)
+    distance = tf.norm(distance, ord=norm, axis=-1) / tf.norm(tf.ones(shape=4), ord=norm)
+    return (distance * valid) + (1.0 - valid)
+
+
+@tf.function(reduce_retracing=True)
+@tf.keras.saving.register_keras_serializable(package='tfe.matchers')
+def compute_matching(y_true_label, y_true_bounding_box, y_pred_label, y_pred_bounding_box, generalized=True, norm=1):
+    label_posteriors = 1.0 - tf.linalg.matmul(y_true_label, y_pred_label, transpose_b=True, a_is_sparse=True)
+    bounding_box_iou = 1.0 - compute_iou(y_true_bounding_box, y_pred_bounding_box, generalized=generalized)
+    bounding_box_distance = compute_distance(y_true_bounding_box, y_pred_bounding_box, norm=norm)
+    cost_matrix = label_posteriors + bounding_box_iou + bounding_box_distance
     return linear_sum_assignment(cost_matrix)
 
 
 @tf.keras.saving.register_keras_serializable(package='tfe.matchers')
 class Hungarian(tf.Module):
-    def __init__(self, mode='giou', name='hungarian'):
+    def __init__(self, generalized=True, norm=1, name='hungarian'):
         super().__init__(name=name)
-        self.mode = mode
+        self.generalized = generalized
+        self.norm = norm
 
     def __call__(self, y_true_label, y_true_bounding_box, y_pred_label, y_pred_bounding_box, padding_axis=None):
         with self.name_scope:
             return tf.stop_gradient(
                 compute_matching(y_true_label=y_true_label, y_true_bounding_box=y_true_bounding_box,
-                                 y_pred_label=y_pred_label, y_pred_bounding_box=y_pred_bounding_box, mode=self.mode))
+                                 y_pred_label=y_pred_label, y_pred_bounding_box=y_pred_bounding_box,
+                                 generalized=self.generalized, norm=self.norm))
 
     @classmethod
     def from_config(cls, config):
@@ -116,5 +132,6 @@ class Hungarian(tf.Module):
 
     def get_config(self):
         return {
-            'mode': self.mode
+            'generalized': self.generalized,
+            'norm': self.norm
         }
