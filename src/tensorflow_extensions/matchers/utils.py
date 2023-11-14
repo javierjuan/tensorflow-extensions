24019e2e45630761744bc978c8d5453a9cdbbce2
javierjuan <javij1@gmail.com>
2023-11-14 09:55:41 +0100

    Reduce re-tracing

--- src/tensorflow_extensions/matchers/utils.py
+++ src/tensorflow_extensions/matchers/utils.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 def index_by_assignment(tensor, assignment):
     assigned_indices = tf.where(assignment)[:, -1]
     unassigned_indices = tf.where(tf.math.reduce_all(tf.math.logical_not(assignment), axis=0))[:, -1]
@@ -9,22 +9,22 @@ def index_by_assignment(tensor, assignment):
     return tf.gather(tensor, indices, axis=0)
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 def gather_by_assignment(tensor, assignment):
     indices = tf.where(assignment)[:, -1]
     return tf.gather(tensor, indices, axis=0)
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 def gather_by_mask(tensor, mask):
     return tf.boolean_mask(tensor, mask=mask, axis=0)
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 def padding_mask(label, padding_axis=-1):
     return tf.cast(label[..., padding_axis], dtype=tf.bool)
 
 
-@tf.function
+@tf.function(reduce_retracing=True)
 def non_padding_mask(label, padding_axis=-1):
     return tf.math.logical_not(padding_mask(label=label, padding_axis=padding_axis))
