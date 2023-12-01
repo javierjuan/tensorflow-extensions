8b70fdb6b5dff38bb322609452135a704ff1d04a
javierjuan <javij1@gmail.com>
2023-12-01 21:58:52 +0100

    * Fix default paramter value

--- src/tensorflow_extensions/models/vit.py
+++ src/tensorflow_extensions/models/vit.py
@@ -49,7 +49,7 @@ class ViT(Model):
                  moving_variance_initializer='ones',
                  mode='convolution',
                  strides=None,
-                 padding='valid',
+                 padding='same',
                  data_format=None,
                  dilation_rate=(1, 1),
                  convolution_groups=1,
