b487685529bbf84eefef49da051c8c4bc9c7f537
javierjuan <javij1@gmail.com>
2024-01-19 09:41:28 +0100

    Several improvements in ViT model * Now can be saved correctly * Added standardization * Added Pooling layer instead of using CLS token in Transformer * Use new PatchAndPositionEmbedding2D layer

--- src/tensorflow_extensions/models/vit.py
+++ src/tensorflow_extensions/models/vit.py
@@ -1,16 +1,15 @@
 import keras_core as keras
 
-from .model import Model
+from ..layers.encoding import PatchAndPositionEmbedding2D
 from ..layers.mlp import MultiLayerPerceptron
-from ..layers.encoding import PatchEmbedding2D
 from ..layers.transformer import TransformerEncoder
 
 
 @keras.saving.register_keras_serializable(package='tfe.models')
-class ViT(Model):
+class ViT(keras.Model):
     def __init__(self,
                  patch_size,
-                 num_classes,
+                 num_labels,
                  embedding_dimension,
                  encoder_units,
                  encoder_num_heads,
@@ -39,9 +38,6 @@ class ViT(Model):
                  embeddings_initializer='uniform',
                  embeddings_regularizer=None,
                  embeddings_constraint=None,
-                 mask_zero=False,
-                 input_length=None,
-                 sparse=False,
                  normalization='layer',
                  momentum=0.99,
                  normalization_groups=32,
@@ -51,22 +47,67 @@ class ViT(Model):
                  strides=None,
                  padding='same',
                  data_format=None,
-                 dilation_rate=(1, 1),
                  convolution_groups=1,
                  rate=None,
                  seed=None,
                  name=None,
                  **kwargs):
         super().__init__(name=name, **kwargs)
-        self.patch_encoding = PatchEmbedding2D(
+        self.patch_size = patch_size
+        self.num_labels = num_labels
+        self.embedding_dimension = embedding_dimension
+        self.encoder_units = encoder_units
+        self.encoder_num_heads = encoder_num_heads
+        self.mlp_units = mlp_units
+        self.use_bias = use_bias
+        self.output_shape = output_shape
+        self.attention_axes = attention_axes
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
+        self.activity_regularizer = activity_regularizer
+        self.kernel_constraint = kernel_constraint
+        self.bias_constraint = bias_constraint
+        self.activation = activation
+        self.axis = axis
+        self.epsilon = epsilon
+        self.center = center
+        self.scale = scale
+        self.beta_initializer = beta_initializer
+        self.gamma_initializer = gamma_initializer
+        self.beta_regularizer = beta_regularizer
+        self.gamma_regularizer = gamma_regularizer
+        self.beta_constraint = beta_constraint
+        self.gamma_constraint = gamma_constraint
+        self.embeddings_initializer = embeddings_initializer
+        self.embeddings_regularizer = embeddings_regularizer
+        self.embeddings_constraint = embeddings_constraint
+        self.normalization = normalization
+        self.momentum = momentum
+        self.normalization_groups = normalization_groups
+        self.moving_mean_initializer = moving_mean_initializer
+        self.moving_variance_initializer = moving_variance_initializer
+        self.mode = mode
+        self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.convolution_groups = convolution_groups
+        self.rate = rate
+        self.seed = seed
+
+        self.is_adapted = False
+
+        self.standardization = keras.layers.Normalization(axis=-1, dtype='float32')
+        self.patch_embedding = PatchAndPositionEmbedding2D(
             size=patch_size, embedding_dimension=embedding_dimension, mode=mode, strides=strides, padding=padding,
-            data_format=data_format, dilation_rate=dilation_rate, convolution_groups=convolution_groups,
-            use_bias=use_bias, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
+            data_format=data_format, convolution_groups=convolution_groups, use_bias=use_bias,
+            kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
             kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
             activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
             bias_constraint=bias_constraint, embeddings_initializer=embeddings_initializer,
-            embeddings_regularizer=embeddings_regularizer, embeddings_constraint=embeddings_constraint,
-            mask_zero=mask_zero, input_length=input_length, sparse=sparse)
+            embeddings_regularizer=embeddings_regularizer, embeddings_constraint=embeddings_constraint, rate=rate,
+            seed=seed)
         self.encoder = TransformerEncoder(
             units=encoder_units, num_heads=encoder_num_heads, use_bias=use_bias, output_shape=output_shape,
             attention_axes=attention_axes, kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
@@ -77,7 +118,7 @@ class ViT(Model):
             beta_regularizer=beta_regularizer, gamma_regularizer=gamma_regularizer, beta_constraint=beta_constraint,
             gamma_constraint=gamma_constraint, rate=rate, seed=seed)
         self.normalization = keras.layers.LayerNormalization(epsilon=epsilon)
-        self.flatten = keras.layers.Flatten()
+        self.pooling = keras.layers.GlobalAveragePooling1D()
         self.mlp = MultiLayerPerceptron(
             units=mlp_units, activation=activation, use_bias=use_bias, kernel_initializer=kernel_initializer,
             bias_initializer=bias_initializer, kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
@@ -89,15 +130,73 @@ class ViT(Model):
             gamma_regularizer=gamma_regularizer, beta_constraint=beta_constraint, gamma_constraint=gamma_constraint,
             axis=axis, rate=rate, seed=seed)
         self.posteriors = keras.layers.Dense(
-            units=num_classes, activation='softmax', use_bias=use_bias, kernel_initializer=kernel_initializer,
+            units=num_labels, activation='sigmoid', use_bias=use_bias, kernel_initializer=kernel_initializer,
             bias_initializer=bias_initializer, kernel_regularizer=kernel_regularizer, bias_regularizer=bias_regularizer,
             activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint,
             bias_constraint=bias_constraint, dtype='float32')
 
+    def adapt(self, data):
+        self.standardization.adapt(data=data)
+        self.is_adapted = True
+
+    def build(self, input_shape):
+        if not self.is_adapted:
+            raise ValueError('Model must be adapted before calling `build` or `call` methods.')
+        super().build(input_shape=input_shape)
+
     def call(self, inputs, training=False, **kwargs):
-        x = self.patch_encoding(inputs)
+        x = self.standardization(inputs)
+        x = self.patch_embedding(x)
         x = self.encoder(x, training=training)
         x = self.normalization(x)
-        x = self.flatten(x)
+        x = self.pooling(x)
         x = self.mlp(x, training=training)
         return self.posteriors(x)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'patch_size': self.patch_size,
+            'num_labels': self.num_labels,
+            'embedding_dimension': self.embedding_dimension,
+            'encoder_units': self.encoder_units,
+            'encoder_num_heads': self.encoder_num_heads,
+            'mlp_units': self.mlp_units,
+            'use_bias': self.use_bias,
+            'output_shape': self.output_shape,
+            'attention_axes': self.attention_axes,
+            'kernel_initializer': self.kernel_initializer,
+            'bias_initializer': self.bias_initializer,
+            'kernel_regularizer': self.kernel_regularizer,
+            'bias_regularizer': self.bias_regularizer,
+            'activity_regularizer': self.activity_regularizer,
+            'kernel_constraint': self.kernel_constraint,
+            'bias_constraint': self.bias_constraint,
+            'activation': self.activation,
+            'axis': self.axis,
+            'epsilon': self.epsilon,
+            'center': self.center,
+            'scale': self.scale,
+            'beta_initializer': self.beta_initializer,
+            'gamma_initializer': self.gamma_initializer,
+            'beta_regularizer': self.beta_regularizer,
+            'gamma_regularizer': self.gamma_regularizer,
+            'beta_constraint': self.beta_constraint,
+            'gamma_constraint': self.gamma_constraint,
+            'embeddings_initializer': self.embeddings_initializer,
+            'embeddings_regularizer': self.embeddings_regularizer,
+            'embeddings_constraint': self.embeddings_constraint,
+            'normalization': self.normalization,
+            'momentum': self.momentum,
+            'normalization_groups': self.normalization_groups,
+            'moving_mean_initializer': self.moving_mean_initializer,
+            'moving_variance_initializer': self.moving_variance_initializer,
+            'mode': self.mode,
+            'strides': self.strides,
+            'padding': self.padding,
+            'data_format': self.data_format,
+            'convolution_groups': self.convolution_groups,
+            'rate': self.rate,
+            'seed': self.seed
+        })
+        return config
