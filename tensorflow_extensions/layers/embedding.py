3c01fc965a2584007623bf39857157ec1de12f25
javierjuan <javij1@gmail.com>
2024-05-13 20:21:17 +0200

    Rewrite FixedEmbedding to work as all Keras layers, receiving a tensor (not a bach_size parameter) and extracting from it the batch_size

--- tensorflow_extensions/layers/embedding.py
+++ tensorflow_extensions/layers/embedding.py
@@ -23,17 +23,11 @@ class FixedEmbedding(keras.layers.Layer):
             shape=(input_dim, output_dim), initializer=embeddings_initializer, regularizer=embeddings_regularizer,
             constraint=embeddings_constraint, trainable=True, name='fixed_embedding')
 
-    def call(self, batch_size=None, **kwargs):
-        if batch_size is None:
-            return self._embedding
-        else:
-            return ops.repeat(ops.expand_dims(self._embedding, axis=0), repeats=batch_size, axis=0)
+    def call(self, inputs, **kwargs):
+        return ops.tile(self._embedding, repeats=(inputs.shape[0], 1, 1))
 
-    def compute_output_shape(self, batch_size=None):
-        if batch_size is None:
-            return self.input_dim, self.output_dim
-        else:
-            return batch_size, self.input_dim, self.output_dim
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self.input_dim, self.output_dim
 
     def get_config(self):
         config = super().get_config()
